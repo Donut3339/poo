6834d4810d1b1fc43fdd6e4e707dbe7cd42251b9
Donut3339 <39208955+Donut3339@users.noreply.github.com>
2020-09-15 13:11:44 +0700

    Create check.php

---
+++ check.php
@@ -0,0 +1,11 @@
+<?php
+$keys = array('key1', 'key2',); // change these to your keys. put in ' s. Seperate with commas (,)
+$sub = $_GET["key"];
+if (in_array($sub,$keys,TRUE)) {
+print('~ True ~');
+}
+else
+{
+print ($_SERVER['REMOTE_ADDR']);
+}
+?>
