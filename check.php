12f38f3a756fac0390036b43ab6a9e6bb0398593
Donut3339 <39208955+Donut3339@users.noreply.github.com>
2020-09-15 13:55:17 +0700

    Update check.php

--- check.php
+++ check.php
@@ -1,5 +1,5 @@
 <?php
-$keys = array('1AfY4Z7', 'Q8auPDI', 'NOxPzkL', 'iWCaMqJ', 'sZv5SAe',); // change these to your keys. put in ' s. Seperate with commas (,)
+$keys = array('free','1AfY4Z7', 'Q8auPDI', 'NOxPzkL', 'iWCaMqJ', 'sZv5SAe',); // change these to your keys. put in ' s. Seperate with commas (,)
 $sub = $_GET["key"];
 if (in_array($sub,$keys,TRUE)) {
 print('Whitelisted');
